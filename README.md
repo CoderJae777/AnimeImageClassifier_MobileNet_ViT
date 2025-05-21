6ddd4a5880c331eca5386049d8923c3cec9adc88
Jae <gohyishen777@gmail.com>
2025-05-21 13:52:45 +0900

    Updated README, gitignore

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # AnimeImageClassifier_MobileNet_ViT
+
+### Setting Up
+```py
+# Navigate to your project folder
+
+# Create a clean virtual environment
+python -m venv venv
+
+# Activate the environment
+venv\Scripts\activate
+
+# Upgrade pip inside the venv
+python -m pip install --upgrade pip setuptools wheel
+
+# Now install your dependencies
+pip install tensorflow-cpu rich markdown-it-py
+```
--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 
 import tensorflow as tf
 from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dropout, Dense
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 import matplotlib.pyplot as plt
+
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 32
+
+print("Hi")
